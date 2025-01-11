94fa091a013586ddabdb12b92815260824535710
johnjamescolgan <jjcolgan@uchicago.edu>
2025-01-11 15:25:30 -0600

    ok, actually fixed :)

--- runMultiCodes.sh
+++ runMultiCodes.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=12
 #SBATCH --mem=12G
 #SBATCH --err=multiCodes.err
-#SBATCH --out=multiCodes.err
+#SBATCH --out=multiCodes.out
 #SBATCH --job-name=multicodes
 #SBATCH --partition=caslake
 #Change lane to which ever run is being analyzed
@@ -12,8 +12,11 @@ mkdir g/lane1/MultiCodeOut
 for sample in `cat samples.txt`
 do
   pigz -d g/lane1/${sample}_L001_R1_001.fastq.gz
+
   perl MultiCodes.pl -out g/lane1/MultiCounts/${sample} \
   -index ${sample} \
-  < bin/g/lane1/${sample}_L001_R1_001.fastq
+  < g/lane1/${sample}_L001_R1_001.fastq \
+  > g/lane1/MultiCodeOut/${sample}.out 2> g/lane1/MultiCodeOut/${sample}.err
+
   pigz g/lane1/${sample}_L001_R1_001.fastq
 done
