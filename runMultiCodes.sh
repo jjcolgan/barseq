e254ecf68b25fde12a635119ecc769c455450540
johnjamescolgan <jjcolgan@uchicago.edu>
2025-01-11 14:53:16 -0600

    bug fixes

---
+++ runMultiCodes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=pi-blekhman
+#SBATCH --ntasks=12
+#SBATCH --mem=12G
+#SBATCH --err=multiCodes.err
+#SBATCH --out=multiCodes.err
+#SBATCH --job-name=multicodes
+#SBATCH --partition=caslake
+#Change lane to which ever run is being analyzed
+mkdir g/lane1/MultiCounts/
+mkdir g/lane1/MultiCodeOut
+for sample in `cat samples.txt`
+do
+  pigz -d g/lane1/${sample}_S18_L001_R1_001.fastq.gz
+  perl MultiCodes -out g/lane1/MultiCounts/${sample} \
+  -index ${sample} \
+  < bin/g/lane1/${sample}_S18_L001_R1_001.fastq> MultiCodeOut/${sample}.out 2> MultiCodeOut/${sample}.err
+  pigz g/lane1/${sample}_S18_L001_R1_001.fastq.gz
+done
